b3c25314c211fe9c8ed2def2a446cf283464c2d7
Ahmed Gemechu <Ahmedgemechu14@gmail.com>
2024-03-04 12:22:19 +0300

    Added productModel

---
+++ BackEnd/Models/productModel.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+const AppError = require("./../utils/appError");
+
+const productSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+  image: [],
+  retailerId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+});
+
+productSchema.pre("save", (next) => {
+  if (this.retailerId.role === "retailer") {
+    next();
+  } else {
+    next(new AppError("You are not allowed to perform this action", 400));
+  }
+});
+// Create the Product model
+const Product = mongoose.model("Product", productSchema);
+
+module.exports = Product;

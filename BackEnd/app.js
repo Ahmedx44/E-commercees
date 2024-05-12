e82e065e6e848fc4871a2b2b03f34023f8e9bbba
AhmedX44 <116306769+Ahmedx44@users.noreply.github.com>
2024-05-13 01:35:55 +0300

    Update app.js

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -18,7 +18,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: "e-commercees-eight.vercel.app",
+    origin: "https://e-commercees-eight.vercel.app",
     methods: ["GET", "POST"],
     Credential: true,
   },

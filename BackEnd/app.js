7c6d389b6f9fbcfea55c869a87136e6ecd298e87
Ahmed Gemechu <ahmedgemechi14@gmail.com>
2024-05-13 02:23:20 +0300

    app js cors updated

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -20,7 +20,7 @@ const io = socketIo(server, {
   cors: {
     origin: "https://e-commercees-a4ree09vw-ahmedx44s-projects.vercel.app",
     methods: ["GET", "POST"],
-    Credential: true,
+    credentials: true, // Correcting typo in 'credentials'
   },
 });
 
@@ -28,6 +28,7 @@ const io = socketIo(server, {
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
+// Use cors middleware to set CORS headers
 app.use(
   cors({
     origin: "https://e-commercees-eight.vercel.app",
@@ -39,11 +40,6 @@ app.use(cookieParser());
 // Set a larger size limit for request bodies (e.g., 50MB)
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
-
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  next();
-});
 
 // Routes
 app.use("/api/users", usersRoute);
